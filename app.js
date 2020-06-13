5448dc525bb28da0600adefa657184c7e1aa0be6
Chaitra Naik <chaitra.naik@ef.com>
2020-06-13 19:26:58 +0200

    added auth mutation and query to frontend

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', grapqlHttp({
@@ -20,11 +30,9 @@ app.use('/graphql', grapqlHttp({
 }));
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@test-4uyiw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
-    .then(() => {
-        app.listen(8000);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-    
-app.listen(3000);
+.then(() => {
+    app.listen(8000);
+})
+.catch(err => {
+    console.log(err);
+});
